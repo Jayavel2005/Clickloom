74ec14702b9aec4295113dbaafd5b2e889b28ae7
Jayavel <jayavel1520@gmail.com>
2026-01-18 20:25:06 +0530

    feat: Server setup

---
+++ server/server.js
@@ -0,0 +1,7 @@
+import express from "express";
+import app from "./app.js";
+import { PORT } from "./config/env.js";
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
